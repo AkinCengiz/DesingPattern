2e4f2fc6f55a40885eb2724a464739b17a6e9e1e
akincengiz <akincengiz@outlook.com.tr>
2023-02-25 15:41:24 +0300

    Created by DesingPatterns project

---
+++ FactoryMethods/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new Log4NetLoggerFactory());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    public class Log4NetLoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new AcLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class AcLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with AcLogger...");
+        }
+    }
+
+    public class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4NetLogger...");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!!!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManeger = CustomerManager.CreateAsSingleton();
+            customerManeger.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object();
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            //=======I. YOL ===============
+            
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+
+            //******************II. YOL**********************
+            //return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!!!");
+        }
+    }
+
+    class EmployeeManager
+    {
+        private static EmployeeManager _employeeManager;
+        static Object _lockObject = new object();
+
+        private EmployeeManager()
+        {
+            
+        }
+
+        public static EmployeeManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_employeeManager == null)
+                {
+                    _employeeManager = new EmployeeManager();
+                }
+            }
+
+            return _employeeManager;
+        }
+    }
+
+    class ProductManager
+    {
+        private static ProductManager _productManager;
+        private static object _lockObject = new object();
+
+        private ProductManager()
+        {
+
+        }
+
+        public static ProductManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_productManager == null)
+                {
+                    _productManager = new ProductManager();
+                }
+            }
+
+            return _productManager;
+        }
+    }
+
+    class OrderManager
+    {
+        private static OrderManager _orderManager;
+        private static object _lockObject = new object();
+
+        private OrderManager()
+        {
+
+        }
+
+        public static OrderManager CreateasSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_orderManager == null)
+                {
+                    _orderManager = new OrderManager();
+                }
+            }
+
+            return _orderManager;
+        }
+    }
+
+}
